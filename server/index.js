c9c9963969b8da882c865f8cbcccca529544a2a3
Wiley Buchanan <swagg187@Wileys-MacBook-Pro.local>
2019-03-01 18:48:18 -0800

    configuring require statements

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('./services/passport');
-
-
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
 const app = express();
 
 //------------- Calling Required authRoutes Function Immediately ------------>>
@@ -13,4 +13,3 @@ mongoose.connect(keys.mongoURI);
 
 app.listen(PORT);
 
-//making a change for git
