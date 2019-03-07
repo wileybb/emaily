315385a6294d1e75a2d242cf4869e1690f88f122
Wiley Buchanan <swagg187@Wileys-MacBook-Pro.local>
2019-03-06 16:28:40 -0800

    creating and configuring seperate dev and prod key sets

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,8 @@
-module.exports = {
-    googleClientID : '57672634953-9mqhngirqpu4r24ffnff8fdrdqb42t3l.apps.googleusercontent.com',
-    googleClientSecret : 'DlDk1VqUl5agFJu-hIXSmrGt',
-    mongoURI : 'mongodb+srv://adam:1234@emaily-iihvb.mongodb.net/test?retryWrites=true',
-    cookieKey: 'abcdefghijklmnopqrstuvwxyz1234567890omgwtfbbq'
-};
+// keys.js - figure out what set of credentials to return
+if(process.env.NODE_ENV === 'production') {
+    // we are in production, return production keys (require and export prod keys)
+    module.exports = require('./prod');
+} else {
+    // we are in development, return dev keys (...requiring dev.js in and exporting those keys in same line...)
+    module.exports = require('./dev');
+}
