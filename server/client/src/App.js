a3864f9c38ab271bc75a09c3c4331bf322e43ef8
Wiley Buchanan <swagg187@Wileys-MacBook-Pro.local>
2019-03-07 12:40:13 -0800

    created react app server, added/configured http proxy middleware

---
+++ server/client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <h2>Welcome to my React App!</h2>
+          <a href="http://localhost:5000/auth/google">Sign In  With Google</a>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
