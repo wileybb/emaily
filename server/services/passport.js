1ef123118d7cc79920f9043ab74b9780dd44588a
Wiley Buchanan <swagg187@Wileys-MacBook-Pro.local>
2019-03-09 15:23:58 -0800

    adding react redux

--- server/services/passport.js
+++ server/services/passport.js
@@ -6,12 +6,12 @@ const keys = require('../config/keys')
 const User = mongoose.model('users');
 
 
-// -- making cookie??
+// -- making cookie
 passport.serializeUser((user, done) => {
     //--- below, user.id is the user number that is assigned by mongo automatically --->>
     done(null, user.id);
 });
-// -- getting data from cookie? 
+// -- getting data from cookie 
 passport.deserializeUser((id, done) => {
     User.findById(id).then(user => {
             done(null, user);
@@ -28,22 +28,19 @@ passport.use(
             proxy: true
         },
         async (accessToken, refreshToken, profile, done) => {
-            const existingUser = await User.findOne({ googleId: profile.id})
-                .then((existingUser) => {
-                    if(existingUser) {
-                     // --- we already have a user with matching google ID in database --->>
-                        return done(null, existingUser);
-        
-                     // --- we don't have this google ID in database, so create a new user instance and save it to database --->>
-                        const user = await new User({ googleId: profile.id}).save()
-                            .then(user => done(null, user));
-                    }
-                })
-
-            console.log('access token: ' + accessToken);
-            console.log('refresh token: ' + refreshToken);
-            console.log('profile: ' + profile);
+            const existingUser = await User.findOne({ googleId: profile.id});
+   
+            if(existingUser) {
+                return done(null, existingUser);
+            }
+            const user = await new User({ googleId: profile.id}).save();
+            done(null, user);
+         console.log('access token: ' + accessToken);
+         console.log('refresh token: ' + refreshToken);
+         console.log('profile: ' + profile);           
         }
+
+        
     )
 );
 
