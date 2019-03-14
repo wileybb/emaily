a9a9ff2fdbc0fcb5fceb9645850fb69e02175396
Wiley Buchanan <swagg187@Wileys-MacBook-Pro.local>
2019-03-14 13:01:00 -0700

    bug fix and refactor asynch await

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>
@@ -8,19 +10,25 @@ const Landing = () => <h2>Landing</h2>
 
 
 //establishing relation between url routes and specific components ---->>
-const App = () => {
-    return(
-        <div>
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route exact path="/" component ={Landing} />
-                    <Route exact path="/surveys" component ={Dashboard} />
-                    <Route path ="/surveys/new" component ={SurveyNew} />
-                </div>
-            </BrowserRouter>
-        </div>
-    );
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
+    render () {
+        return(
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component ={Landing} />
+                        <Route exact path="/surveys" component ={Dashboard} />
+                        <Route path ="/surveys/new" component ={SurveyNew} />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
 };
 
-export default App
+export default connect(null, actions)(App);
